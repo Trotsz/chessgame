01b03da703c0e902deca67b78ceeee547a384bbf
João Pedro <jpviolao@hotmail.com>
2023-07-26 01:39:28 -0300

    New class Position.java created; Modified Program.java to do tests

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,11 @@
 package application;
 
+import models.entities.boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        
+        Position pos = new Position(0, 3);
+
+        System.out.println(pos);
     }
 }
---
+++ src/models/entities/boardgame/Position.java
@@ -0,0 +1,29 @@
+package models.entities.boardgame;
+
+public class Position {
+    private Integer row;
+    private Integer column;
+
+    public Position() {}
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+    public int getRow() {
+        return this.row;
+    }
+
+    public int getColumn() {
+        return this.column;
+    }
+    public void setValues(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        return "Row: " + this.row + ", Column: " + this.column;
+    }
+}
